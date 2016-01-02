5d08019c8f9939a6efe9475db05795015a48415e
jeprodev <jeprodev@gmail.com>
2016-01-02 10:24:01 +0100

    from lap

--- src/com/jeprolab/controllers/JeproLabDashboardController.java
+++ src/com/jeprolab/controllers/JeproLabDashboardController.java
@@ -3,37 +3,29 @@ package com.jeprolab.controllers;
 
 import com.jeprolab.JeproLab;
 import javafx.fxml.FXML;
-import javafx.scene.control.ToggleButton;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class JeproLabDashboardController extends JeproLabController{
-    public ToggleGroup toggleButtonGroup;
-    public ToggleButton monthButton, dayButton, yearButton, previousMonthButton, previousDayButton, previousYearButton;
+    public Button monthButton, dayButton, yearButton, previousMonthButton, previousDayButton, previousYearButton;
     @FXML
     Pane dashboardPageWrapper;
 
     @Override
     public void initialize(URL location, ResourceBundle resource){
         bundle = resource;
-        toggleButtonGroup = new ToggleGroup();
-        dayButton = new ToggleButton(bundle.getString("JEPROLAB_DAY_LABEL"));
-        dayButton.setToggleGroup(toggleButtonGroup);
+        dayButton = new Button(bundle.getString("JEPROLAB_DAY_LABEL"));
         dayButton.getStyleClass().addAll("first");
-        monthButton = new ToggleButton(bundle.getString("JEPROLAB_MONTH_LABEL"));
-        monthButton.setToggleGroup(toggleButtonGroup);
-        yearButton = new ToggleButton(bundle.getString("JEPROLAB_YEAR_LABEL"));
-        yearButton.setToggleGroup(toggleButtonGroup);
-        previousMonthButton = new ToggleButton(bundle.getString("JEPROLAB_PREVIOUS_MONTH_LABEL"));
-        previousMonthButton.setToggleGroup(toggleButtonGroup);
-        previousDayButton = new ToggleButton(bundle.getString("JEPROLAB_PREVIOUS_DAY_LABEL"));
-        previousDayButton.setToggleGroup(toggleButtonGroup);
-        previousYearButton = new ToggleButton(bundle.getString("JEPROLAB_PREVIOUS_YEAR_LABEL"));
+        monthButton = new Button(bundle.getString("JEPROLAB_MONTH_LABEL"));
+
+        yearButton = new Button(bundle.getString("JEPROLAB_YEAR_LABEL"));
+        previousMonthButton = new Button(bundle.getString("JEPROLAB_PREVIOUS_MONTH_LABEL"));
+        previousDayButton = new Button(bundle.getString("JEPROLAB_PREVIOUS_DAY_LABEL"));
+        previousYearButton = new Button(bundle.getString("JEPROLAB_PREVIOUS_YEAR_LABEL"));
         previousYearButton.getStyleClass().addAll("last", "capsule");
-        previousYearButton.setToggleGroup(toggleButtonGroup);
 
         JeproLab.getInstance().getApplicationToolBarCommandWrapper().getChildren().setAll(dayButton, monthButton, yearButton, previousDayButton, previousMonthButton, previousYearButton);
     }
