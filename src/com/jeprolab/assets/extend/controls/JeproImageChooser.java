801aa4575b93683339ddb77764a8a78ecb44a44f
jeproQxT <jeprodev@gmail.com>
2016-01-11 20:21:58 +0100

    login form with style

---
+++ src/com/jeprolab/assets/extend/controls/JeproImageChooser.java
@@ -0,0 +1,10 @@
+package com.jeprolab.assets.extend.controls;
+
+import javafx.scene.layout.Pane;
+
+/**
+ *
+ * Created by jeprodev on 11/01/2014.
+ */
+public class JeproImageChooser extends Pane {
+}
--- src/com/jeprolab/controllers/JeproLabCategoryController.java
+++ src/com/jeprolab/controllers/JeproLabCategoryController.java
@@ -1,7 +1,22 @@
 package com.jeprolab.controllers;
 
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class JeproLabCategoryController extends JeproLabController {
+    @FXML
+    public TableView categoryTableView;
+    public TableColumn categoryIndexColumn, categoryCheckBoxColumn, categoryStatusColumn, categoryNameColumn;
+    public TableColumn categoryDescriptionColumn, categoryPositionColumn, categoryActionColumn;
+    public void initialize(URL location, ResourceBundle resource){
+        bundle = resource;
+    }
 
     @Override
     public void updateToolBar(){}
