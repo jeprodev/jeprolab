9503264b77bdfa6cbaea119d6f66f1b7333ba048
jeprodev <jeprodev@gmail.com>
2016-01-13 07:59:22 +0100

    Sample matrix list editor 1

--- src/com/jeprolab/controllers/JeproLabAnalyzeAddController.java
+++ src/com/jeprolab/controllers/JeproLabAnalyzeAddController.java
@@ -5,8 +5,14 @@ import com.jeprolab.assets.extend.controls.JeproFormPanel;
 import com.jeprolab.assets.extend.controls.JeproFormPanelContainer;
 import com.jeprolab.assets.extend.controls.JeproFormPanelTitle;
 import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.geometry.VPos;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -23,20 +29,26 @@ public class JeproLabAnalyzeAddController extends JeproLabController{
     public JeproFormPanelTitle jeproLabAddAnalyseFormTitleWrapper;
     public JeproFormPanelContainer jeproLabAddAnalyseFormContainerWrapper;
 
+    public GridPane jeproLabAnalyzeInformationLayout;
+
+    public TabPane jeproLabAnalyzeTabPane;
     public Tab jeproLabAnalyzeInformationTabForm, jeproLabAnalyzeAttachedFileTabForm;
 
     public Label jeproLabAnalyzeNameLabel, jeproLabAnalyzePublishedLabel, jeproLabAnalyzeReferenceLabel, jeproLabAnalyzeImageChooserLabel;
     public Label jeproLabAnalyzeShortDescriptionLabel, jeproLabAnalyzeDescriptionLabel, jeproLabAnalyzeImagesLabel, jeproLabAnalyzeTagLabel;
+
+    public TextArea jeproLabAnalyzeShortDescription, jeproLabAnalyzeDescription;
 
     public void initialize(URL location, ResourceBundle resource){
         bundle = resource;
 
         double labelColumnWidth = 150;
         double inputColumnWidth = 300;
-        double formWidth = 2 *(labelColumnWidth + inputColumnWidth) + 30;
+        double formWidth = 0.92 * JeproLab.APP_WIDTH;
         double centerGrid = (formWidth - (labelColumnWidth + inputColumnWidth))/2;
         double posX = (JeproLab.APP_WIDTH/2) - (formWidth)/2;
         double posY = 25;
+        double columnConstraintWidth = (formWidth / 4) - 10;
 
         jeproLabFormTitle = new Label(bundle.getString("JEPROLAB_ADD_NEW_ANALYSE_LABEL"));
         jeproLabFormTitle.getStyleClass().add("input-label");
@@ -48,7 +60,49 @@ public class JeproLabAnalyzeAddController extends JeproLabController{
         jeproLabAddAnalyseFormContainerWrapper.setPrefWidth(0.96 * JeproLab.APP_WIDTH);
         jeproLabAddAnalyseFormContainerWrapper.setLayoutY(40);
 
+        jeproLabAnalyzeTabPane.setPrefWidth(0.96 * JeproLab.APP_WIDTH);
+
+        jeproLabAnalyzeInformationLayout.getColumnConstraints().addAll(
+                new ColumnConstraints(columnConstraintWidth), new ColumnConstraints(columnConstraintWidth),
+                new ColumnConstraints(columnConstraintWidth), new ColumnConstraints(columnConstraintWidth)
+        );
+        jeproLabAnalyzeInformationLayout.setLayoutX(10);
+
+        /*** Tab Information **/
         jeproLabAnalyzeInformationTabForm.setText(bundle.getString("JEPROLAB_INFORMATION_LABEL"));
+        GridPane.setMargin(jeproLabAnalyzeNameLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzePublishedLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeReferenceLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeImageChooserLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeShortDescriptionLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeDescriptionLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeImagesLabel, new Insets(5, 10, 5, 15));
+        GridPane.setMargin(jeproLabAnalyzeTagLabel, new Insets(5, 10, 30, 15));
+
+        jeproLabAnalyzeNameLabel.setText(bundle.getString("JEPROLAB_ANALYSE_NAME_LABEL"));
+        jeproLabAnalyzeNameLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzePublishedLabel.setText(bundle.getString("JEPROLAB_PUBLISHED_LABEL"));
+        jeproLabAnalyzePublishedLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeReferenceLabel.setText(bundle.getString("JEPROLAB_REFERENCE_LABEL"));
+        jeproLabAnalyzeReferenceLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeImageChooserLabel.setText(bundle.getString("JEPROLAB_CHOOSE_IMAGE_LABEL"));
+        jeproLabAnalyzeImageChooserLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeShortDescriptionLabel.setText(bundle.getString("JEPROLAB_SHORT_DESCRIPTION_LABEL"));
+        GridPane.setValignment(jeproLabAnalyzeShortDescriptionLabel, VPos.TOP);
+        jeproLabAnalyzeShortDescriptionLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeDescriptionLabel.setText(bundle.getString("JEPROLAB_DESCRIPTION_LABEL"));
+        GridPane.setValignment(jeproLabAnalyzeDescriptionLabel, VPos.TOP);
+        jeproLabAnalyzeDescriptionLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeImagesLabel.setText(bundle.getString("JEPROLAB_IMAGES_LABEL"));
+        jeproLabAnalyzeImagesLabel.getStyleClass().add("input-label");
+        jeproLabAnalyzeTagLabel.setText(bundle.getString("JEPROLAB_TAG_LABEL"));
+        jeproLabAnalyzeTagLabel.getStyleClass().add("input-label");
+
+        GridPane.setMargin(jeproLabAnalyzeShortDescription, new Insets(10, 0, 0, 0));
+        jeproLabAnalyzeShortDescription.setPrefHeight(55);
+        GridPane.setMargin(jeproLabAnalyzeDescription, new Insets(10, 0, 0, 0));
+        jeproLabAnalyzeDescription.setPrefHeight(85);
         jeproLabAnalyzeAttachedFileTabForm.setText(bundle.getString("JEPROLAB_ATTACHED_FILES_LABEL"));
+
     }
 }
